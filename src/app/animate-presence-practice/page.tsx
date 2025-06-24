4dd751f11fc73615d6d10308fd8b304c251a9d66
ShimJiHoon <wlgns2223@gmail.com>
2025-06-24 13:47:18 +0900

    chore: v0.3.0 animate presnece header

--- src/app/animate-presence-practice/page.tsx
+++ src/app/animate-presence-practice/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import AnimatePresencePractice from "@/components/practice-templates/04-animate-presence-practice";
+import Navigation from "../../components/Navigation";
 
 export default function AnimatePresencePracticePage() {
-  return <AnimatePresencePractice />;
+  return (
+    <>
+      <Navigation title="4. Animate Presence - 실습" />
+      <AnimatePresencePractice />;
+    </>
+  );
 }
